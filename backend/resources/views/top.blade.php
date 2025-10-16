d5d431e8c2ccc736d335937cf0bc9c82cf61cc82
城戸一馬 <jodo@MacBook.local>
2025-10-16 14:36:25 +0900

    header css

---
+++ backend/resources/views/top.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+
+@section('content')
+    <h2>トップページ</h2>
+    
+@endsection
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,8 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 
 Route::get('/', function () {
-    return "hello world";
+    return view('top');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
 
 Route::get('/articles', [ArticleController::class, 'index']);
 Route::get('/articles/create', [ArticleController::class, 'create']);
