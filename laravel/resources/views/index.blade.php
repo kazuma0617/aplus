287f33f2f35d73e93b89fa4e47e5ec05ebb7390b
城戸一馬 <jodo@MacBook.local>
2025-11-11 11:10:08 +0900

    一覧ページのCSS

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -1,7 +1,31 @@
 @extends('layout')
 
 @section('content')
-<div class="container" style="max-width: 800px; margin-top: 40px;">
+<div class="main-contents">
+    <h1>アプレンティス生オリジナルプロダクト</h1>
+    @foreach($articles as $article)
+    <div class="article-card">
+        <div class="article-left">
+            <img src="{{ $article['image_path'] 
+                ? asset('storage/' . $article['image_path']) 
+                : asset('images/default.jpg') }}" 
+                alt="記事画像" class="article-image">
+        </div>
+        <div class="article-right">
+            <a href="{{ route('articles.show', $article->id) }}"><h3>{{ $article->title }}</h3></a>
+            <p>
+                @foreach($article->tags as $tag)
+                    <span class="tag">#{{ $tag->name }}</span>
+                @endforeach
+            </p>
+            <p>投稿者：{{ optional($article->user)->name ?? '不明なユーザー' }}</p>
+        </div>
+    </div>
+    @endforeach
+</div>
+@endsection
+
+<!-- <div class="container" style="max-width: 800px; margin-top: 40px;">
     <h2>記事一覧</h2>
 
     @if($articles->isEmpty())
@@ -20,5 +44,4 @@
             </div>
         @endforeach
     @endif
-</div>
-@endsection
+</div> -->
