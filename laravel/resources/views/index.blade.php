5076a98deb4ff5cca147b3dd4b61dc5d915b4de4
城戸一馬 <jodo@MacBook.local>
2025-11-20 16:54:55 +0900

    一覧ページを２列で表示

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <div class="main-contents">
-    <h1>アプレンティス生オリジナルプロダクト</h1>
     @foreach($articles as $article)
     <div class="article-card">
         <div class="article-left">
@@ -12,18 +11,22 @@
                 alt="記事画像" class="article-image">
         </div>
         <div class="article-right">
-            <a href="{{ $article->url }}"><h3>{{ $article->title }}</h3></a>
-            @if ($article->github_url)
-            <a href="{{ $article->github_url }}" class="github-btn" target="_blank">
-                <i class="bi bi-github"></i>
-            </a>
-            @endif
+            <h3><a href="{{ $article->url }}">{{ $article->title }}</a></h3>
+            
+            
             <p>
                 @foreach($article->tags as $tag)
                     <span class="tag">#{{ $tag->name }}</span>
                 @endforeach
             </p>
-            <p>投稿者：{{ optional($article->user)->name ?? '不明なユーザー' }}</p>
+            <p class="poster-line">
+                @if ($article->github_url)
+                <a href="{{ $article->github_url }}" class="github-btn" target="_blank">
+                    <i class="bi bi-github"></i>
+                </a>
+                @endif
+                投稿者：{{ optional($article->user)->name }}
+            </p>
         </div>
     </div>
     @endforeach
