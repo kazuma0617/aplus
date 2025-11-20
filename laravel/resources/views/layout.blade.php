8c72bf35d35c0cbe941dbdc08291e97df708fd18
城戸一馬 <jodo@MacBook.local>
2025-11-20 18:39:45 +0900

    pagination実装

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Aplus</title>
@@ -21,7 +22,7 @@
         
     </header>
 
-    <main class="container">
+    <main>
         @yield('content')
     </main>
 </body>
